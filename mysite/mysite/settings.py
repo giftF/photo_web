9934b1f7eca74b6c53d221d57a27cb0baaf11867
giftF <1432002@qq.com>
2018-12-01 15:21:21 +0800

    ssl

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'photo',
+    'werkzeug_debugger_runserver',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
