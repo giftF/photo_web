d94a01edcc6476ad3e87ba753264a44ac6508ca1
giftF <1432002@qq.com>
2018-12-05 11:45:05 +0800

    连接mysql数据库

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'test',
-        # 'USER': 'gift',
-        # 'PASSWORD': '123456',
-        # 'HOST': '18.191.190.29',
-        # 'PORT': '3306',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'minidata',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
         # 'OPTIONS':{
         #     'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
         #     'charset':'utf8mb4',
